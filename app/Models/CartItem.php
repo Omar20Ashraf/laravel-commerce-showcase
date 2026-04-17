65aea6e2cebaeaab11e33948951030b3e6a89f43
Omar20Ashraf <o.ashraf@solutionplus.net>
2026-04-18 00:15:25 +0200

    wip: add has time zone to cart item

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\HasTimezoneFields;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -9,7 +10,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CartItem extends Model
 {
-    use HasFactory;
+    use HasFactory, HasTimezoneFields;
 
     protected $fillable = [
         'user_id',
