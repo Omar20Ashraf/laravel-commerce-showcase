78c0e5f0944c402c2f303d856d5d23a857bd62e4
Omar20Ashraf <o.ashraf@solutionplus.net>
2026-04-17 14:02:38 +0200

    enhance: modules seeder data

--- app/Http/Requests/Website/RegisterRequest.php
+++ app/Http/Requests/Website/RegisterRequest.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Requests\Website;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class RegisterRequest extends FormRequest
